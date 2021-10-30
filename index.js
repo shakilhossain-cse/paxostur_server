0cea45d3984cc64e87d8e969c4b1f7afe3b1e914
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-10-31 00:09:07 +0600

    database connection url setup

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ require('dotenv').config();
 const app = express()
 const port =process.env.PORT || 5000
 
+// middleware 
 app.use(cors())
 app.use(express.json())
+
+// database connection 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@shakil.qlmoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
