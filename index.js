257ae05bf55a509cbe2b17f1b4dc4dd47c6a40e4
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-10-31 15:31:46 +0600

    insert api and show data api create

--- index.js
+++ index.js
@@ -1,24 +1,73 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
 
-const app = express()
-const port =process.env.PORT || 5000
 
-// middleware 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-// database connection 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@shakil.qlmoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("AmazingPark");
+    const ridesCollection = database.collection("Rides");
+    // create a document to insert
+    app.post("/addride", async (req, res) => {
+      const ride = req.body;
+      const result = await ridesCollection.insertOne(ride);
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      res.json(result);
+    });
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    // get document from database
+      app.get("/allride", async (req, res) => {
+          const cursor = ridesCollection.find({});
+          const rides = await cursor.toArray();
+        
+        
+      // print a message if no documents were found
+      if ((await cursor.count()) === 0) {
+        console.log("No documents found!");
+        }
+        res.send(rides)
+    });
+
+    // app.delete("/ride/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   console.log(id);
+    //   const query = { _id:ObjectId(id) };
+    // const result = await ridesCollection.deleteOne(query);
+    //   res.json(id)
+    // })
+
+    app.get("/ride/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id:ObjectId(id) };
+      const Ride = await ridesCollection.findOne(query);
+      res.json(Ride)
+    })
+
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 app.listen(port, () => {
-  console.log(`Your server is running at http://localhost:${port}`)
-})
+  console.log("server started in ", port);
+});
